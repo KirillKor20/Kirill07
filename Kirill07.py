be27422b8d9a9de4de07d13bc78639ffa6fc35c5
KirillKor20 <vwer7743@mail.ru>
2024-05-30 05:37:54 +0300

    Add files via upload

---
+++ Kirill07.py
@@ -0,0 +1,16 @@
+immutable_var_ = 123,414,523,312 , 'homework', 'new_york'
+immutable_var_2 = (123,414,523,312,True)
+immutable_var_3 = tuple([123,414,523,312, 'Tomato'])
+immutable_var_4 = list(['Mazda','Toyota','Ferrari'])
+print(immutable_var_)
+print(immutable_var_2)
+print(immutable_var_3)
+print(immutable_var_4)
+mutable_list = ([42,34,98,],125,'Power_Bank') + (242,503)
+print(mutable_list)
+mutable_list[0][2]= 3
+print( mutable_list)
+mutable_list = (182,290) * 6
+print( mutable_list)
+
+
